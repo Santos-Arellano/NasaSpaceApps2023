f2182e154c4d7081454503aa68cc8f5d5f51617a
Santos Alejandro Arellano Olarte <133918146+Santos-Arellano@users.noreply.github.com>
2023-10-07 13:35:26 -0600

    page.js deployed

---
+++ explorador-sistema-solar/sistema-solar-ui/src/app/planetas.js
@@ -0,0 +1,13 @@
+// pages/Planetas.js
+import React from 'react';
+
+function Planetas() {
+  return (
+    <div>
+      <h1>Lista de Planetas</h1>
+      {/* Agrega contenido relacionado con los planetas aquí */}
+    </div>
+  );
+}
+
+export default Planetas;
