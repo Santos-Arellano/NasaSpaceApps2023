5e95213bb6b00eb96c008b55200f45057052df21
Fernando Antonio López García <A01643685@tec.mx>
2023-10-07 17:14:27 -0600

    update planets

--- planevoyage-app/src/App.js
+++ planevoyage-app/src/App.js
@@ -6,6 +6,11 @@ const planets = [
   { name: 'Mercury', imagePath: '/images/mercurio.png'},
   { name: 'Venus', imagePath: '/images/Venus.png' },
   { name: 'Earth', imagePath: '/images/earth.png' },
+  { name: 'Mars', imagePath: '/images/Mars.png' },
+  { name: 'Jupiter', imagePath: '/images/Jupiter.png' },
+  { name: 'Saturn', imagePath: '/images/saturno.png' },
+  { name: 'Uranus', imagePath: '/images/urano.png' },
+  { name: 'Neptune', imagePath: '/images/neptuno.png' },
   // Agrega el resto de los planetas aquí
 ];
 
