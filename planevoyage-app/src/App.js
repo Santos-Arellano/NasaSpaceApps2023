fde7e08c3ac84d6aec55343b5288863a560b6e59
Santos Alejandro Arellano Olarte <133918146+Santos-Arellano@users.noreply.github.com>
2023-10-08 20:27:54 -0600

    design1

--- planevoyage-app/src/App.js
+++ planevoyage-app/src/App.js
@@ -1,12 +1,40 @@
-import React from 'react';
+// App.js
+import React, { useState } from 'react';
 import './App.css';
 import PlanetViewer from './planetViewer';
 
-const App = () => {
+const planets = [
+  { name: 'Mercury', imagePath: '/images/mercurio.png', description: 'The closest planet to the Sun.' },
+  { name: 'Venus', imagePath: '/images/Venus.png', description: 'An extremely hot planet with a thick atmosphere.' },
+  { name: 'Earth', imagePath: '/images/earth.png', description: 'Our home, the only known planet with life.' },
+];
+
+function App() {
+  const [currentPlanetIndex, setCurrentPlanetIndex] = useState(0);
+
+  const nextPlanet = () => {
+    setCurrentPlanetIndex((prevIndex) => (prevIndex + 1) % planets.length);
+  };
+
+  const previousPlanet = () => {
+    setCurrentPlanetIndex((prevIndex) => (prevIndex - 1 + planets.length) % planets.length);
+  };
+
+  const planet = planets[currentPlanetIndex];
+
   return (
     <div className="App">
-      <h1>Planetary Voyage</h1>
-      <PlanetViewer />
+      <header className="App-header">
+        <h1>Planetary Tourism</h1>
+        <p>Explore the Solar System</p>
+      </header>
+      <PlanetViewer
+        planetName={planet.name}
+        imagePath={planet.imagePath}
+        description={planet.description}
+        onNextClick={nextPlanet}
+        onPreviousClick={previousPlanet}
+      />
     </div>
   );
 }
